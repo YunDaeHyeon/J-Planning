7d10239a43e8c5c8f776087f01fea419875e3750
DaeHyeon <kayuaao96@gmail.com>
2022-03-18 12:18:45 +0900

    2022-03-18 create retrofit package

---
+++ app/src/main/java/com/meonjicompany/planning/retrofit/UserInformationInterface.java
@@ -0,0 +1,10 @@
+package com.meonjicompany.planning.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface UserInformationInterface {
+
+    // @GET("/users")
+    // Call<List<Post>> getData(@Query("userId") String id);
+}
